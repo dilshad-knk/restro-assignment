dbeafab6750802ac7a6716966ebe02db82dfe2dc
dilshad-knk <dilshadali.knk@gmail.com>
2024-12-15 06:31:18 +0000

    cookie-parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import bodyParser from "body-parser";
+import cookieParser from 'cookie-parser';
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./database";
@@ -13,6 +14,8 @@ app.use(cors({
     credentials: true ,
     origin: "https://restro-assignment.vercel.app"
 }));
+
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use("/api/v1", menuRoutes);
