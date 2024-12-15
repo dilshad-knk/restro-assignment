f61fb4a47e7151d6ef121267846d52b6d6b3cc7c
dilshad-knk <dilshadali.knk@gmail.com>
2024-12-15 04:58:58 +0000

    up

--- frontend/src/axios/axios.ts
+++ frontend/src/axios/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    // baseURL: import.meta.env.VITE_BASE_URL,
-  baseURL: "http://localhost:5000/api/v1",
+    baseURL: import.meta.env.VITE_BASE_URL,
+//   baseURL: "http://localhost:5000/api/v1",
 withCredentials: true,
 });
 
